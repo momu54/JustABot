6cc459794382bc56980f3879fdfe019ff4cf058b
MuMapleTW <xiaozhi.tw.kh@gmail.com>
2022-07-22 10:34:07 +0800

    把程式碼變得更乾淨

--- commands/ping.ts
+++ commands/ping.ts
@@ -1,28 +1,35 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonInteraction, ButtonStyle, CommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js"
+import {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonInteraction,
+    ButtonStyle,
+    CommandInteraction,
+    EmbedBuilder,
+    SlashCommandBuilder,
+} from "discord.js";
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("ping")
         .setDescription("show websocket ping"),
     execute: (i: CommandInteraction) => {
-        var embed = getpingembed(i)
+        var embed = getpingembed(i);
         var button = new ButtonBuilder()
-            .setCustomId('ping.refresh')
-            .setLabel('Refresh')
-            .setStyle(ButtonStyle.Primary)
-        var row = new ActionRowBuilder<ButtonBuilder>()
-            .addComponents(button)
-        i.reply({ embeds: [embed], components: [row] })
+            .setCustomId("ping.refresh")
+            .setLabel("Refresh")
+            .setStyle(ButtonStyle.Primary);
+        var row = new ActionRowBuilder<ButtonBuilder>().addComponents(button);
+        i.reply({ embeds: [embed], components: [row] });
     },
     executeBtn: (i: ButtonInteraction) => {
-        var embed = getpingembed(i)
-        i.update({ embeds: [embed] })
-    }
-}
+        var embed = getpingembed(i);
+        i.update({ embeds: [embed] });
+    },
+};
 
-function getpingembed (i: CommandInteraction | ButtonInteraction): EmbedBuilder {
+function getpingembed(i: CommandInteraction | ButtonInteraction): EmbedBuilder {
     return new EmbedBuilder()
         .setColor(0x000000)
         .setTitle("Pong!")
-        .setDescription(`${i.client.ws.ping} ms`)
+        .setDescription(`${i.client.ws.ping} ms`);
 }
--- index.ts
+++ index.ts
@@ -1,60 +1,81 @@
-import { Client, Collection, GatewayIntentBits, InteractionType, SlashCommandBuilder } from 'discord.js'
-import 'dotenv/config'
-import path from 'path'
-import fs from 'fs'
+import {
+    Client,
+    Collection,
+    ComponentType,
+    GatewayIntentBits,
+    InteractionType,
+    SlashCommandBuilder,
+} from "discord.js";
+import "dotenv/config";
+import path from "path";
+import fs from "fs";
 
-const client = new Client({ intents: [GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates] })
-var commands = new Collection()
-const commandsPath = path.join(__dirname, 'commands')
-const commandsFile = fs.readdirSync(commandsPath)
-var commandslist: SlashCommandBuilder[] = []
+const client = new Client({
+    intents: [
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildVoiceStates,
+    ],
+});
+var commands = new Collection();
+const commandsPath = path.join(__dirname, "commands");
+const commandsFile = fs.readdirSync(commandsPath);
+var commandslist: SlashCommandBuilder[] = [];
 
 interface CommandType {
-    data: SlashCommandBuilder,
-    execute: Function,
-    executeBtn?: Function
+    data: SlashCommandBuilder;
+    execute: Function;
+    executeBtn?: Function;
 }
 
 client.on("ready", () => {
-    console.log('ready!')
-    loadcommand()
-})
+    console.log("ready!");
+    loadcommand();
+});
 
 async function loadcommand() {
     for (const file of commandsFile) {
-        const filePath = path.join(commandsPath, file)
-        const command = require(filePath) as CommandType
-        commands.set(command.data.name, command)
-        commandslist.push(command.data)
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath) as CommandType;
+        commands.set(command.data.name, command);
+        commandslist.push(command.data);
     }
-    console.log('Started refreshing application (/) commands.');
-    await client.application?.commands.set(commandslist)
-    console.log('Successfully reloaded application (/) commands.');
+    console.log("Started refreshing application (/) commands.");
+    await client.application?.commands.set(commandslist);
+    console.log("Successfully reloaded application (/) commands.");
 }
 
-client.on('interactionCreate', async (i) => {
+client.on("interactionCreate", async (i) => {
     if (i.type == InteractionType.ApplicationCommand) {
         const commandFile = commands.get(i.commandName) as CommandType;
-        
+
         if (!commandFile) return;
 
         try {
             await commandFile.execute(i);
         } catch (error) {
             console.error(error);
-            i.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+            i.reply({
+                content: "There was an error while executing this command!",
+                ephemeral: true,
+            });
         }
     }
     if (i.type == InteractionType.MessageComponent) {
-        const commandName = i.customId.split('.')[0]
-        const commandFile = commands.get(commandName) as CommandType
-        try {
-            await commandFile.executeBtn?.(i)
-        } catch (error) {
-            console.error(error);
-            i.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        if (i.componentType == ComponentType.Button) {
+            const commandName = i.customId.split(".")[0];
+            const commandFile = commands.get(commandName) as CommandType;
+            try {
+                await commandFile.executeBtn?.(i);
+            } catch (error) {
+                console.error(error);
+                i.reply({
+                    content: "There was an error while executing this command!",
+                    ephemeral: true,
+                });
+            }
         }
     }
 });
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
