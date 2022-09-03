1137e717aebdf2bce9345c24c795a9fb17f60805
某幕 <96234201+MoMuAlt@users.noreply.github.com>
2022-09-03 09:22:46 +0800

    終於完成儲存圖片

--- MessageCommands/Save all image.ts
+++ MessageCommands/Save all image.ts
@@ -7,6 +7,7 @@ import {
 } from 'discord.js';
 import JSZip from 'jszip';
 import jimp from 'jimp';
+import { writeFileSync } from 'fs';
 
 module.exports = {
 	data: new ContextMenuCommandBuilder()
@@ -21,25 +22,34 @@ module.exports = {
 			const Attachment = AttachmentList[index];
 			if (!Attachment.contentType?.startsWith('image')) continue;
 			const res = await fetch(Attachment.url);
-			var resimage = await res.arrayBuffer();
+			var resimage = Buffer.from(await res.arrayBuffer());
 			if (Attachment.contentType != 'image/jpeg') {
-				const jimpimage = await jimp.read(Buffer.from(resimage));
-				resimage = (await jimpimage.getBufferAsync('image/jpeg')).buffer;
+				const jimpimage = await jimp.read(resimage);
+				jimpimage.quality(70);
+				resimage = await jimpimage.getBufferAsync('image/jpeg');
 			}
-			zip.file(`${index + 1}.${Attachment.name}`, resimage, {
+			zip.file(`${index + 1}.${Attachment.name?.replace('png', 'jpg')}`, resimage, {
 				binary: true,
 				compression: 'DEFLATE',
 			});
 		}
 		const zipData = await zip.generateAsync({ type: 'nodebuffer' });
+		if (zipData.byteLength > 8388608) {
+			const errembed = new EmbedBuilder()
+				.setColor(0xff0000)
+				.setTitle('error!')
+				.setDescription('The generated zip is too large.');
+			await i.editReply({ embeds: [errembed] });
+			return;
+		}
 		const zipAttachment = new AttachmentBuilder(zipData, {
-			name: `${new Date().toTimeString()}`,
+			name: `${new Date().toTimeString()}.${msg.id}.zip`,
 		});
 		const size = (zipData.byteLength * 0.000001).toFixed(2);
 		const embed = new EmbedBuilder()
 			.setColor(0xffffff)
-			.setTitle('Done!')
+			.setTitle('done!')
 			.setDescription(`size: ${size}MB`);
-		i.editReply({ embeds: [embed], files: [zipAttachment] });
+		await i.editReply({ embeds: [embed], files: [zipAttachment] });
 	},
 };
