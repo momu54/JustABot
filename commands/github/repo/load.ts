23ce9131bffba5f21dca08cf99c3c365eabeb5a5
某幕 <96234201+MoMuAlt@users.noreply.github.com>
2022-11-02 20:35:51 +0800

    修復404 crash 修復crash錯誤

--- commands/github/repo/load.ts
+++ commands/github/repo/load.ts
@@ -14,46 +14,55 @@ export async function loadrepo(values: string[]) {
 		owner: repoowner,
 	});
 	// 取得 readme
-	const readme: any = await appoctokit.rest.repos.getReadme({
-		repo: reponame,
-		owner: repoowner,
-		mediaType: {
-			format: 'html',
-		},
-	});
-	// 啟動樓覽器
-	const browser = await launch({
-		defaultViewport: {
-			width: 1200,
-			height: 1000,
-		},
-		args: ['--no-sandbox'],
-	});
-	// 開啟新分頁
-	const page = await browser.newPage();
-	// 載入 html
-	await page.setContent(stylehtml + readme.data);
-	// 截圖
-	const img = await page.screenshot({
-		quality: 100,
-		type: 'webp',
-		encoding: 'binary',
-	});
-	// 建構附件
-	const attachment = new AttachmentBuilder(img, { name: 'readme.webp' });
+	const readme: any = await appoctokit.rest.repos
+		.getReadme({
+			repo: reponame,
+			owner: repoowner,
+			mediaType: {
+				format: 'html',
+			},
+		})
+		.catch((err) => {
+			return {
+				data: '',
+				status: err.status,
+			};
+		});
 	// 建立 embed
 	const embed = new EmbedBuilder()
 		.setTitle(data.full_name)
 		.setDescription(data.description)
-		.setImage(readme.data.content)
 		.setAuthor({ name: data.owner.login, iconURL: data.owner.avatar_url })
-		.setImage('attachment://readme.webp')
 		.setColor(0xffffff);
 	// 回傳資料
 	const msgplayload: WebhookEditMessageOptions = {
 		embeds: [embed],
-		files: [attachment],
 		components: [],
 	};
+	if (readme.status == 200) {
+		// 啟動樓覽器
+		const browser = await launch({
+			defaultViewport: {
+				width: 1200,
+				height: 1000,
+			},
+			args: ['--no-sandbox'],
+		});
+		// 開啟新分頁
+		const page = await browser.newPage();
+		// 載入 html
+		await page.setContent(stylehtml + readme.data);
+		// 截圖
+		const img = await page.screenshot({
+			quality: 100,
+			type: 'webp',
+			encoding: 'binary',
+		});
+		// 建構附件
+		const attachment = new AttachmentBuilder(img, { name: 'readme.webp' });
+		// 加入附件
+		msgplayload.files = [attachment];
+		embed.setImage('attachment://readme.webp');
+	}
 	return msgplayload;
 }
